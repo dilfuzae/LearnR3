eb7346e51de4e419bc06138f73cf903cbd09cb00
Dilfuza Ernafasova <ernafasova.dilfuza@gmail.com>
2024-05-07 12:12:02 +0200

    exercise with importing multiple data also using functions

--- R/functions.R
+++ R/functions.R
@@ -4,7 +4,7 @@
 #'
 #' @return Output of imported info data
 import_user_info <- function(file_path) {
-  info_data <- readr::read_csv( #add dependency package here
+  info_data <- readr::read_csv( # add dependency package here
     file_path,
     col_select = -1, # col skip (deletes the column)
     col_types = readr::cols(
@@ -18,7 +18,6 @@ import_user_info <- function(file_path) {
   )
 
   return(info_data)
-
 }
 
 
@@ -73,4 +72,47 @@ import_actigraph <- function(file_path) {
   )
   return(actigraph_data)
 }
+
+
+
+#' Import the MMASH saliva file.
+#'
+#' @param file_path Path to the user saliva data file.
+#'
+#' @return Outputs a data frame/tibble.
+#'
+import_saliva <- function(file_path) {
+  saliva_data <- readr::read_csv(
+    file_path,
+    col_select = -1,
+    col_types = readr::cols(
+      samples = readr::col_character(),
+      cortisol_norm = readr::col_double(),
+      melatonin_norm = readr::col_double()
+    ),
+    name_repair = snakecase::to_snake_case
+  )
+  return(saliva_data)
 }
+
+# Test that this works
+# import_saliva(here("data-raw/mmash/user_1/saliva.csv"))
+
+
+#' Title Importing multiple files at the same time using rbind
+#'
+#' @param file_pattern
+#' @param import_function
+#'
+#' @return Output of imported multiple files
+import_multiple_files <- function(file_pattern, import_function) {
+  data_files <- fs::dir_ls(here("data-raw/mmash/"),
+    regexp = file_pattern,
+    recurse = TRUE
+  )
+  combined_data <- purrr::map(data_files, import_function) |>
+    purrr::list_rbind(names_to = "file_path_id")
+  return(combined_data)
+}
+
+
